35cec4f2f4c50fc2a3aa42cbf73fea0ef47ebbe3
Olha Brailovska <obrailovska@Olhas-MacBook-Air.local>
2022-01-18 16:19:35 -0500

    added more drag-drop functionallity

--- assets/js/script.js
+++ assets/js/script.js
@@ -183,6 +183,20 @@ $("#task-form-modal .btn-primary").click(function () {
     saveTasks();
   }
 });
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function (event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function (event, ui) {
+    console.log("over");
+  },
+  out: function (event, ui) {
+    console.log("out");
+  },
+});
 
 // remove all tasks
 $("#remove-tasks").on("click", function () {
